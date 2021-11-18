4a48b7f2e884695ab58db7f6da51244f209abe36
Mark Tasaka <mark.tasaka@gmail.com>
2021-11-18 09:37:08 -0800

    Update 1.04

--- character2/php/hitPoints.php
+++ character2/php/hitPoints.php
@@ -4,8 +4,40 @@
 function getHitPoints ($level, $conMod)
 {
     $hitPoints = 1;
+
+    if($level == 1)
+    {
+        $hitDice = 1;
+    }
+    else if($level == 2)
+    {
+        $hitDice = 2;
+    }
+    else if($level == 3 || $level == 4)
+    {
+        $hitDice = 3;
+    }
+    else if($level == 5)
+    {
+        $hitDice = 4;
+    }
+    else if($level == 6)
+    {
+        $hitDice = 5;
+    }
+    else if($level == 7 || $level == 8)
+    {
+        $hitDice = 6;
+    }
+    else if($level == 9)
+    {
+        $hitDice = 7;
+    }
+    else
+    {
+        $hitDice = 8;
+    }
     
-    $hitDice = $level;
     
     $counter = 0;
             
@@ -23,7 +55,16 @@ function getHitPoints ($level, $conMod)
                 $hitPoints += $hpEachLevel;
             }
 
+    if($level == 4)
+    {
+        $hitPoints += 1;
+    }
     
+    if($level == 8)
+    {
+        $hitPoints += 1;
+    }
+
     return $hitPoints;
     
 }
@@ -34,7 +75,7 @@ function getHitDiceAmount ($level)
     
     if($level == 1)
         {
-            $hd = "1+1";
+            $hd = "1";
         }
     else if($level == 2)
         {
@@ -46,31 +87,31 @@ function getHitDiceAmount ($level)
         }
     else if($level == 4)
         {
-            $hd = "4";
+            $hd = "3+1";
         }
     else if($level == 5)
         {
-            $hd = "5";
+            $hd = "4";
         }
     else if($level == 6)
         {
-            $hd = "6";
+            $hd = "5";
         }
     else if($level == 7)
         {
-            $hd = "7";
+            $hd = "6";
         }
     else if($level == 8)
         {
-            $hd = "8";
+            $hd = "6+1";
         }
     else if($level == 9)
         {
-            $hd = "9";
+            $hd = "7";
         }
     else if($level == 10)
         {
-            $hd = "10";
+            $hd = "8";
         }
     
     return $hd;
